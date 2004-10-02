0d14c1d408733069bfb38430b13a7ca03163dff3
robert <>
2004-10-02 07:11:56 +0000

    loops through all domains

--- anyhosting.com/htdocs/clients/billingCheck.php
+++ anyhosting.com/htdocs/clients/billingCheck.php
@@ -1,14 +1,20 @@
 #!/usr/bin/php4 -q
 <?php
   include_once('billing.inc');
-  
   $today = date("m/d/Y");
-  $domain='customweather.com';
-  $currentCycle = currentCycle($domain);
-
-  foreach($currentCycle as $c){
-    if($today == $c['dueDate']){
-      print "$domain is due today.\n";
+  $filename = "/var/www/DOMAINS.list";
+  $handle = fopen($filename,"r");
+  $contents = fread($handle,filesize($filename));
+  fclose($handle);
+  $lines = explode("\n", $contents);
+  foreach($lines as $domain){
+    if(!empty($domain)){
+      $currentCycle = currentCycle($domain);
+      if(!empty($currentCycle)) foreach($currentCycle as $c){
+        if($today == $c['dueDate']){
+          print "$domain is due today.\n";
+        }
+      }
     }
-  }
+  }    
 ?>
