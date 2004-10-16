948ac8dba3ce5f8580ea53ffeb8cc6d01638045b
robert <>
2004-10-16 10:08:44 +0000

    correct links

--- anyhosting.com/htdocs/clients/services/index.php
+++ anyhosting.com/htdocs/clients/services/index.php
@@ -3,9 +3,9 @@
   include_once('header.inc');
 ?>
     <ul>
-      <li><a href="/clients/email.php?domain=<?=$domain?>">Email Administration</a></li>
+      <li><a href="/clients/services/email?domain=<?=$domain?>">Email Administration</a></li>
       <li><a href="http://<?=$domain?>/webalizer">Usage Statistics</a></li>
-      <li><a href="/clients/billing.php?domain=<?=$domain?>">Billing Info</a></li>
+      <li><a href="/clients/services/billing?domain=<?=$domain?>">Billing Info</a></li>
     </ul>
     <p><a href="/contact">Contact us</a><br>
     Copyright © 2004 AnyHosting Services<br>
