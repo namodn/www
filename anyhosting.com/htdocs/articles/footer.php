f402290b8158f0c179bdcf88ba503c7a55c006f5
rhelmer <>
2007-09-09 17:51:22 +0000

    modify ad placement

--- anyhosting.com/htdocs/articles/footer.php
+++ anyhosting.com/htdocs/articles/footer.php
@@ -3,7 +3,25 @@
     <p><a href="/contact">Contact us</a> if you have any questions or 
     suggestions, or would like to contribute an article.</p>
     </div>
-    <?php include 'ads.php'?>
+    <br>
+    <script type="text/javascript"><!--
+      google_ad_client = "pub-3713787574379441";
+      google_ad_width = 728;
+      google_ad_height = 90;
+      google_ad_format = "728x90_as";
+      google_ad_type = "text";
+      //2007-05-10: AnyHosting Articles
+      google_ad_channel = "1667581840";
+      google_color_border = "666666";
+      google_color_bg = "FFFFFF";
+      google_color_link = "0000FF";
+      google_color_text = "000000";
+      google_color_url = "000000";
+    //-->
+    </script>
+    <script type="text/javascript"
+     src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+    </script>
     <p>&copy;2007 AnyHosting</p>
   </body>
 </html>
