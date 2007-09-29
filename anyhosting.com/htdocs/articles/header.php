52f256d9a4f38024b03bd64d7a54f92cc0497592
rhelmer <>
2007-09-29 18:09:42 +0000

    remove header ad

--- anyhosting.com/htdocs/articles/header.php
+++ anyhosting.com/htdocs/articles/header.php
@@ -8,22 +8,4 @@
   </head>
 
   <body>
-<script type="text/javascript"><!--
-google_ad_client = "pub-3713787574379441";
-google_ad_width = 468;
-google_ad_height = 60;
-google_ad_format = "468x60_as";
-google_ad_type = "text_image";
-//2007-09-06: AnyHosting Articles
-google_ad_channel = "1667581840";
-google_color_border = "666666";
-google_color_bg = "FFFFFF";
-google_color_link = "0000FF";
-google_color_text = "000000";
-google_color_url = "000000";
-//-->
-</script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
     <div id="main">
