9bd432e958b7c737b4ca4ce919a29593f6100ff3
rhelmer <>
2008-02-24 08:47:33 +0000

    better ads

--- anyhosting.com/htdocs/articles/header.php
+++ anyhosting.com/htdocs/articles/header.php
@@ -8,4 +8,15 @@
   </head>
 
   <body>
+<script type="text/javascript"><!--
+google_ad_client = "pub-3713787574379441";
+/* articles header */
+google_ad_slot = "8704214361";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
     <div id="main">
