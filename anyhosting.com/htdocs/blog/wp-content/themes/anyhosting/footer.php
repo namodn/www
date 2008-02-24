5426038130d671f5fef1dfbf43119a1ff6dc26a4
rhelmer <>
2008-02-24 08:47:18 +0000

    check in current blog setup

---
+++ anyhosting.com/htdocs/blog/wp-content/themes/anyhosting/footer.php
@@ -0,0 +1,31 @@
+
+<hr />
+<div id="footer">
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+	<p>
+		<?php bloginfo('name'); ?> is proudly powered by
+		<a href="http://wordpress.org/">WordPress</a>
+		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
+		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</p>
+</div>
+</div>
+
+<!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
+<?php /* "Just what do you think you're doing Dave?" */ ?>
+
+		<?php wp_footer(); ?>
+<script type="text/javascript"><!--
+google_ad_client = "pub-3713787574379441";
+/* blog footer */
+google_ad_slot = "7190048133";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</body>
+</html>
